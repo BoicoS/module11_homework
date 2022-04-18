6694c55164041893667d5f4aaaed597e682cd7b2
Uliana <boicos06@gmail.com>
2022-04-18 09:36:51 +0300

    mod11

--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,16 @@
-import {getMonth} from "./index.js";
+import {getPercents} from "./index.js";
 
-describe('test for getMonth function', ()=> {
-    const nmrMonth = 7;
-    const less = 0;
-    const more = 13;
-    const string = 'month';
+describe('test for getPercents function', ()=> {
+    const percent = 20;
+    const number = 100;
+    const floatPerc= 20.5;
+    const floatNum = 100.5;
+    const numStr = "a";
+    
 
-    it('correct output input data', ()=> {
-        expect(getMonth(nmrMonth)).toBe('июль');
-        expect(getMonth(less)).toBe('неизвестно');
-        expect(getMonth(more)).toBe('неизвестно');
-        expect(getMonth(string)).toBe('неизвестно');
-    })
-})
+    it('correct output input data', ()=> 
+        expect(getPercents(percent, number)).toBe(20));
+        expect(getPercents(floatPerc, floatNum)).toBe(20.6025);        
+        expect(getPercents(percent, numStr)).toBe("Input values are not numbers");       
+    
+});
